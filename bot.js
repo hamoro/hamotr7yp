5ed1ff68853943a578efda2ec8e941d75269d558
hamoro <40411863+hamoro@users.noreply.github.com>
2018-09-09 06:49:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,71 +1,47 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('guildMemberAdd', member => {
-const channel = member.guild.channels.find("name","ask")
-if (member.user.bot) return;
-var Canvas = require('canvas-prebuilt')  
-var jimp = require('jimp')
-  var currentTime = new Date(),
-hours = currentTime.getHours() + 3 ,
-minutes = currentTime.getMinutes(),
-seconds = currentTime.getSeconds();
-if (minutes < 10) {
-minutes = "0" + minutes;
-}
-var suffix = "AM";
-if (hours >= 12) {
-suffix = "PM";
-hours = hours - 12;
-}
-if (hours == 0) {
-hours = 12;
-}
- 
- 
-const w = ['./img/20180806_230413.png'];
+client.on("guildMemberAdd", member => {
+      let starcode = member.guild.channels.find("name","ask");
+      if(!starcode) return;
+      if(starcode) {
+     
+         
+       var Canvas = require('canvas')
+      var jimp = require('jimp')
+     
+      const w = ['./img/20180806_230413.png'];
+     
               let Image = Canvas.Image,
                   canvas = new Canvas(557, 241),
                   ctx = canvas.getContext('2d');
+ 
               fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
                   if (err) return console.log(err)
                   let BG = Canvas.Image;
                   let ground = new Image;
                   ground.src = Background;
                   ctx.drawImage(ground, 0, 0, 557, 241);
- 
+     
       })
+     
                       let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
                       jimp.read(url, (err, ava) => {
                           if (err) return console.log(err);
                           ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
                               if (err) return console.log(err);
- 
-                                    ctx.font = '30px Arial Bold';
+     
+                                    ctx.font = '25px Arial Bold';
                               ctx.fontSize = '20px';
                               ctx.fillStyle = "#FFFFFF";
                                 ctx.fillText(member.user.username, 300, 120);
- 
-                                    ctx.font = '24px Arial Bold';
-                              ctx.fontSize = '15px';
-                              ctx.fillStyle = "#FFFFFF";
-                                ctx.fillText(`${hours}:${minutes}:${seconds} ${suffix}`, 300, 160);
-                                              var guild;
- 
- 
- 
- 
- 
-                                                 //NAMEً
+                             
+                              //NAMEً
                               ctx.font = '25px Arial';
                               ctx.fontSize = '28px';
                               ctx.fillStyle = "#FFFFFF";
       ctx.fillText(`Welcome To ${member.guild.name}`, 260, 60);
- 
+     
                               //AVATARً
                               let Avatar = Canvas.Image;
                               let ava = new Avatar;
@@ -78,9 +54,15 @@ const w = ['./img/20180806_230413.png'];
  
                         ctx.drawImage(ava, 49, 17, 200, 200);
                               ctx.closePath();
- channel.sendFile(canvas.toBuffer())
-                          })
-})
-});
+                           
+    starcode.sendFile(canvas.toBuffer())
+     
+ 
+     
+      })
+      })
+     
+      }
+      });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
